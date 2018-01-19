8e4ebf7c6e027878bdf1ece42d30be7fa14882b5
saupetit <saupetit@MTP-D320-004.DIGINAMIC.LOCAL>
2018-01-19 16:58:34 +0100

    Mise a jour du fichier sql contenant les données

--- src/main/resources/creation_objects.sql
+++ src/main/resources/creation_objects.sql
@@ -1,19 +1,34 @@
-INSERT INTO product (activated, description, image, orders, price, stock, title, category)
+INSERT INTO product (id, activated, description, image, orders, price, stock, title, category)
  VALUES
  
- (true, 'La vie de l''enfance de Cerise, de Groupama','carnet-cerise', 0, 15.90, 4, 'Les carnets de Cerise', 'Livre'),
- (true, 'Les meilleurs vins de bordeaux en BD', 'chateaux-bordeaux', 0, 22.30, 14, 'Châteaux Bordeaux', 'Livre'),
- (true, 'Le renouveaux de la lingerie en France', 'culottees', 0, 16.20, 9, 'Culottées : des femmes qui font ce qu''elles veulent quand elles veulent', 'Livre'),
- (true, 'Une histoire d''amour poignante', 'et-si-l-amour-c-etait-aimer', 0, 7.80, 5, 'Et si l''amour c''était aimer ?', 'Livre'),
- (true, 'Les félin n''ont jamais été si intelligent', 'le-chat-du-rabin', 0, 10.80, 10, 'Le chat du rabin', 'Livre'),
- (true, 'Encire une histoire de sous vêtement', 'le-loup-en-slip', 0, 17.80, 7, 'Le loup en slip', 'Livre'),
- (true, 'Un enfant insupportable malgré toutes les réprimandes qu''il se prend régulièrement', 'le-petit-nicolas', 0, 20.80, 5, 'Le petit Nicolas', 'Livre'),
- (true, 'L''âme de l''artiste est palpable parmi les ombres', 'les-amours-supendues', 0, 18.50, 13, 'Les amours suspendues', 'Livre'),
- (true, 'Une BD a lire quand on est dans la Lune, mais les pieds sur Terre', 'thomas-pesquet', 0, 19.99, 14, 'Dans la combi de Thomas Pesquet', 'Livre'),
- (true, 'A lire absolument ', 'un-autre-regard', 0, 15.75, 5, 'Un autre regard', 'Livre'),
- (true, 'A ne pas confondre avec le Dr Mamour de Grey''s Anatomy', 'Dr-Folamour', 0, 10.75, 15, 'Docteur Folamour', 'DVD'),
- (true, 'Les rideaux ne sont pas la que pour la déco', 'Hitchcock-Fenetre-sur-cour', 0, 11.75, 5, 'Fenêtre sur cour', 'DVD'),
- (true, 'Le plagiat c''est pas bien', 'Hitchcock-La-main-au-collet', 0, 15.75, 5, 'La main au collet', 'DVD'),
- (true, 'Melanie, jeune femme quelque peu superficielle, rencontre chez un marchand d''oiseaux un brillant et séduisant avocat qui recherche des inséparables.', 'Hitchcock-Les-oiseaux', 0, 12.75, 5, 'Les oiseaux', 'DVD'),
- (true, 'Scottie est sujet au vertige, ce qui lui porte préjudice dans son métier de policier. Rendu responsable de la mort d''un de ses collègues, il décide de quitter la police. Une ancienne relation le contacte afin qu''il suive sa femme, possédée selon lui par l''esprit de son aïeule. Scottie s''éprend de la jeune femme et se trouve ballotté par des évènements qu''il ne peut contrôler.', 'Hitchcock-Sueurs-froides', 0, 12.75, 5, 'Sueurs froides', 'DVD'),
- (true, 'Au cœur de Los Angeles, une actrice en devenir prénommée Mia sert des cafés entre deux auditions. De son côté, Sebastian, passionné de jazz, joue du piano dans des clubs miteux pour assurer sa subsistance', 'La-La-Land-DVD', 0, 18.75, 5, 'La La Land', 'DVD');
+ (nextval('idsequence'), true, 'La vie de l''enfance de Cerise, de Groupama','carnet-cerise', 0, 15.90, 4, 'Les carnets de Cerise', 'Livre'),
+ (nextval('idsequence'), true, 'Les meilleurs vins de bordeaux en BD', 'chateaux-bordeaux', 0, 22.30, 14, 'Châteaux Bordeaux', 'Livre'),
+ (nextval('idsequence'), true, 'Le renouveaux de la lingerie en France', 'culottees', 0, 16.20, 9, 'Culottées : des femmes qui font ce qu''elles veulent quand elles veulent', 'Livre'),
+ (nextval('idsequence'), true, 'Une histoire d''amour poignante', 'et-si-l-amour-c-etait-aimer', 0, 7.80, 5, 'Et si l''amour c''était aimer ?', 'Livre'),
+ (nextval('idsequence'), true, 'Les félin n''ont jamais été si intelligent', 'le-chat-du-rabin', 0, 10.80, 10, 'Le chat du rabin', 'Livre'),
+ (nextval('idsequence'), true, 'Encire une histoire de sous vêtement', 'le-loup-en-slip', 0, 17.80, 7, 'Le loup en slip', 'Livre'),
+ (nextval('idsequence'), true, 'Un enfant insupportable malgré toutes les réprimandes qu''il se prend régulièrement', 'le-petit-nicolas', 0, 20.80, 5, 'Le petit Nicolas', 'Livre'),
+ (nextval('idsequence'), true, 'L''âme de l''artiste est palpable parmi les ombres', 'les-amours-supendues', 0, 18.50, 13, 'Les amours suspendues', 'Livre'),
+ (nextval('idsequence'), true, 'Une BD a lire quand on est dans la Lune, mais les pieds sur Terre', 'thomas-pesquet', 0, 19.99, 14, 'Dans la combi de Thomas Pesquet', 'Livre'),
+ (nextval('idsequence'), true, 'A lire absolument ', 'un-autre-regard', 0, 15.75, 5, 'Un autre regard', 'Livre'),
+ (nextval('idsequence'), true, 'A ne pas confondre avec le Dr Mamour de Grey''s Anatomy', 'Dr-Folamour', 0, 10.75, 15, 'Docteur Folamour', 'DVD'),
+ (nextval('idsequence'), true, 'Les rideaux ne sont pas la que pour la déco', 'Hitchcock-Fenetre-sur-cour', 0, 11.75, 5, 'Fenêtre sur cour', 'DVD'),
+ (nextval('idsequence'), true, 'Le plagiat c''est pas bien', 'Hitchcock-La-main-au-collet', 0, 15.75, 5, 'La main au collet', 'DVD'),
+ (nextval('idsequence'), true, 'Melanie, jeune femme quelque peu superficielle, rencontre chez un marchand d''oiseaux un brillant et séduisant avocat qui recherche des inséparables.', 'Hitchcock-Les-oiseaux', 0, 12.75, 5, 'Les oiseaux', 'DVD'),
+ (nextval('idsequence'), true, 'Scottie est sujet au vertige, ce qui lui porte préjudice dans son métier de policier. Rendu responsable de la mort d''un de ses collègues, il décide de quitter la police. Une ancienne relation le contacte afin qu''il suive sa femme, possédée selon lui par l''esprit de son aïeule. Scottie s''éprend de la jeune femme et se trouve ballotté par des évènements qu''il ne peut contrôler.', 'Hitchcock-Sueurs-froides', 0, 12.75, 5, 'Sueurs froides', 'DVD'),
+ (nextval('idsequence'), true, 'Au cœur de Los Angeles, une actrice en devenir prénommée Mia sert des cafés entre deux auditions. De son côté, Sebastian, passionné de jazz, joue du piano dans des clubs miteux pour assurer sa subsistance', 'La-La-Land-DVD', 0, 18.75, 5, 'La La Land', 'DVD'),
+ (nextval('idsequence'), true, 'La plus courte rencontre du cinéma', 'Hitchcock-Les-39-marches', 0, 16.85, 7, 'Les 39 marches', 'DVD'),
+ (nextval('idsequence'), true, 'Alicia, fille d''un espion nazi, mène une vie dépravée. Devlin lui propose de travailler pour les Etats-Unis afin de réhabiliter son nom.', 'Hitchcock-Les-enchaines', 0, 15.00, 8, 'Les enchainés', 'DVD'),
+ (nextval('idsequence'), true, 'Un classique indescriptible', 'Shining', 0, 13.85, 80, 'Shining', 'DVD'),
+ (nextval('idsequence'), true, 'La révolte comme on aimerait la voir plus souvent', 'Spartacus', 0, 11.30, 4, 'Spartacus', 'DVD'),
+ (nextval('idsequence'), true, 'De l''Indochine bien comme on l''aime', '13-edition-Standard-Digisleeve', 0, 16.85, 2, 'Indochine - Les enfants', 'CD'),
+ (nextval('idsequence'), true, 'RIP', '50-Plus-Belles-Chansons', 0, 16.85, 7, 'Johnny - le best-of post thune', 'CD'),
+ (nextval('idsequence'), true, 'Digne de son père, toujours impressionnant de technique et de poêsie', 'Amour-Chien-Fou', 0, 9.99, 7, 'Arthur H - Amour Chien Fou', 'CD'),
+ (nextval('idsequence'), true, 'Incontournable série de chansons plus admirables les unes que les autres', 'Au-cafe-du-canal', 0, 16.85, 1, 'Les Ogres de Barback - Au café du canal', 'CD'),
+ (nextval('idsequence'), true, 'Enfin France Gall pas trop jeune', 'france-gall', 0, 24.99, 5, 'France Gall', 'CD'),
+ (nextval('idsequence'), true, 'Un des pilliers de la chanson Française', 'la-meme-tribu', 0, 16.85, 6, 'Eddy Mitchell', 'CD'),
+ (nextval('idsequence'), true, 'Encore un pillier de la chanson Française', 'Libertine', 0, 16.85, 7, 'Mylene Farmer', 'CD');
+
+
+
+
