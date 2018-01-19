7aac0a42d22b1e87609e9d1b2a6aa39a0197eeb3
Arnaud BONNET <abonnet@DIGINAMIC.LOCAL>
2018-01-19 09:46:37 +0100

    initial commit

---
+++ src/main/java/fr/dta/scah/ScahApplication.java
@@ -0,0 +1,12 @@
+package fr.dta.scah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScahApplication.class, args);
+	}
+}
---
+++ src/main/java/fr/dta/scah/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.dta.scah;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ScahApplication.class);
+	}
+
+}
