9984f51c92827f6110d86edfb6e37764ce4463e9
Hamida SIDI SAID OMAR <hsidisaidomar@DIGINAMIC.LOCAL>
2018-01-22 13:30:13 +0100

    modif du , du chemin

--- src/main/webapp/app/js/service/ProductService.js
+++ src/main/webapp/app/js/service/ProductService.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ProductService', ['$http', function($http) {
 	
 	var host = 'http://localhost' ;
 	var port = '8080';
-	var promiseProducts = $http.get(host + ':' + port + "/scah/api/public/products");
+	var promiseProducts = $http.get(host + ':' + port + "/scah/api/products");
 	
 	var getAllProductBody = function() {
 		var promiseListProduct = promiseProducts.then(function(response) {
