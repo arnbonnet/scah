dfd5a6dbb8ba9558eb49a67fca0ad06f926b5234
saupetit <saupetit@MTP-D320-004.DIGINAMIC.LOCAL>
2018-01-22 15:02:24 +0100

    JavaScript : mise à jour du app.js pour ajouter une route.

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,7 +1,14 @@
-angular.module('app', ['ngRoute']).config(function($routeProvider) {
+var app = angular.module('app', ['ngRoute']);
+
+
+app.config(function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl:'./app/template/list_product.html',
-		controller:'ListProductCtrl'
+		controller:'ListProductCtrl',
+	})
+	.when('/Admin_products', {
+			templateUrl : './app/template/Array_products_admin.html',
+			controller : 'ArrayProductsAdminController',
 	})
 	.otherwise({
 		redirectTo:'/'
