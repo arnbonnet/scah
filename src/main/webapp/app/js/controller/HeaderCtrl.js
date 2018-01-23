291da934c97dd2cbd28c5df9457ddb86d5ddfe22
Arnaud BONNET <abonnet@DIGINAMIC.LOCAL>
2018-01-23 15:46:45 +0100

    menu v1 : avec tous les affichages ensemble visiteur/utilisateur/admin

---
+++ src/main/webapp/app/js/controller/HeaderCtrl.js
@@ -0,0 +1,11 @@
+angular.module('app').controller('HeaderCtrl', function($scope/*, UserService*/) {
+	
+	$scope.role;
+	
+//	UserService.getRole().then(function(data) {
+//		$scope.role = data;
+//	},
+//	function() {
+//		console.log("Error HeaderCtrl - getRole");
+//	});
+});
