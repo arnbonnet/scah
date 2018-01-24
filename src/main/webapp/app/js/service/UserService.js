20893d251c4257854fc1cad9235c59af4fcd8d3d
Arnaud BONNET <abonnet@DIGINAMIC.LOCAL>
2018-01-24 16:12:13 +0100

    connection admin/user nav updated

--- src/main/webapp/app/js/service/UserService.js
+++ src/main/webapp/app/js/service/UserService.js
@@ -1,35 +1,56 @@
 angular.module('app').factory('UserService', ['$http', '$location',function($http, $location) {
+	var role = '';
 	
-	var getRoleBody = function() {
-		var promiseUser = $http.get('/scah/api/users/connectedUser');
-		var promiseRole = promiseUser.then(function(response) {
+	function calculateRole(){ 
+		return $http.get('/scah/api/users/connectedUser').then(function(response) {
 			console.log(response.data);
 			if(response.data.admin) {
-				return 'admin';
+				role = 'admin';
 			} else if(response.data.admin === false) {
-				return 'user';
+				role = 'user';
 			} else {
-				return '';
+				role = '';
 			}
 		});
-		return promiseRole;
+	}
+
+	var getRoleBody = function() {
+		return role;
 	};
 	
 	var loginBody = function(email, password) {
 		var loginPromise = $http.post('/scah/authenticate', undefined, {params:{username:email, password:password}});
 		loginPromise.then(
 				function() {
-					$location.path('/scah');					
+					return calculateRole().then(function(){
+						$location.path('/scah');
+					}); // Le loggage est effeciflorsuqe l'on a également rechargé le role
 				},
 				function() {
 					console.log('error UserService - login');
+					role = '';
 				}
 		);
 	};
 	
+	var logoutBody = function() {
+		var logoutPromise = $http.post('/scah/logout');
+		logoutPromise.then(
+				function() {
+					$location.path('/scah#!logout');
+					return calculateRole(); // Le déloggage est effeciflorsuqe l'on a également rechargé le role
+				},
+				function() {
+					console.log('error UserService - logout');
+					return calculateRole(); // Le déloggage est effeciflorsuqe l'on a également rechargé le role
+				}
+		);
+	}
+	
 	return {
-		//getRole : getRoleBody,
-		login : loginBody
+		getRole : getRoleBody,
+		login : loginBody,
+		logout : logoutBody
 	}
 }]);
 
