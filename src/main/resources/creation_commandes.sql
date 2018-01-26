24ee9247c70d3011748ff31ce76d1b434e7bc751
Sapho AUPETIT <saupetit@DIGINAMIC.LOCAL>
2018-01-26 14:32:37 +0100

    RESOURCES - creation d'un fichier sql de commandes

---
+++ src/main/resources/creation_commandes.sql
@@ -0,0 +1,4 @@
+insert into _order (id,date, order_number,total_price, user_id) values
+(nextval('seq_user'), '2017-25-08', '46484392937', '10.3', 1),
+(nextval('seq_user'), '2017-12-02', '89789466322', '57.3', 1),
+(nextval('seq_user'), '2017-01-01', '87895842155', '82.5', 1);
